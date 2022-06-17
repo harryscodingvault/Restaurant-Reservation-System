8e9ce1cef0c0d62f6f8da4a8ed20dbdb683e275b
HaroldDRF <95robots@gmail.com>
2022-06-16 22:59:31 -0700

    Menu style added

---
+++ front-end/src/layout/NotFound.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  color: red;
+  background-color: var(--grey-700);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Wrapper;
