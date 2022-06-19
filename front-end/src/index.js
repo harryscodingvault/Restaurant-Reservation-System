9a6fb3b96e0fc708f3587cd7c578bf4256ceceda
HaroldDRF <95robots@gmail.com>
2022-06-18 23:27:06 -0700

    react-redux-toolkit store added

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
