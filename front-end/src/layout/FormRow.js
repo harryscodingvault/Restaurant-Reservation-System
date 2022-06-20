48efe36d6d4c040b72bcdc93681cfb8cbe12a125
HaroldDRF <95robots@gmail.com>
2022-06-20 02:40:10 -0700

    add reservation form added

--- front-end/src/layout/FormRow.js
+++ front-end/src/layout/FormRow.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Wrapper from "./FormRow.style.js";
 
-const FormRow = () => {
-  return <div>FormRow</div>;
+const FormRow = ({
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  min,
+  pattern,
+}) => {
+  return (
+    <div className="form-row">
+      <input
+        className="form-input"
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        min={min}
+        pattern={pattern}
+      ></input>
+    </div>
+  );
 };
 
 export default FormRow;
--- front-end/src/pages/addReservation/addReservation.js
+++ front-end/src/pages/addReservation/addReservation.js
@@ -1,40 +1,81 @@
 import React, { useState } from "react";
 import Wrapper from "./addReservation.style.js";
+import FormRow from "../../layout/FormRow.js";
+
+import { today } from "../../utils/date-time.js";
 
 const initialValues = {
   first_name: "",
   last_name: "",
   mobile_number: "",
-  reservation_date: "",
+  reservation_date: today(),
   reservation_time: "",
+  people: 1,
 };
 
 const AddReservation = () => {
   const [values, setValues] = useState(initialValues);
 
   const handleChange = (e) => {
-    console.log(e);
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name, value);
+    setValues({ ...values, [name]: value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+    console.log(values);
   };
 
   return (
     <Wrapper>
       <form className="form" onSubmit={onSubmit}>
         <h3>Add Reservation</h3>
-        <div className="form-row">
-          <input
-            type="text"
-            className="form-input"
-            name="first_name"
-            placeholder="First Name"
-            value={values.first_name}
-            onChange={handleChange}
-          ></input>
-        </div>
+        <FormRow
+          type="text"
+          name="first_name"
+          placeholder="First Name"
+          value={values.first_name}
+          handleChange={handleChange}
+        ></FormRow>
+        <FormRow
+          type="text"
+          name="last_name"
+          placeholder="Last Name"
+          value={values.last_name}
+          handleChange={handleChange}
+        ></FormRow>
+        <FormRow
+          type="tel"
+          name="mobile_number"
+          placeholder="Mobile Number"
+          value={values.mobile_number}
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          handleChange={handleChange}
+        ></FormRow>
+        <FormRow
+          type="date"
+          name="reservation_date"
+          placeholder="Reservation Date"
+          value={values.reservation_date}
+          handleChange={handleChange}
+        ></FormRow>
+        <FormRow
+          type="time"
+          name="reservation_time"
+          placeholder="Reservation Time"
+          value={values.reservation_time}
+          handleChange={handleChange}
+        ></FormRow>
+        <FormRow
+          type="number"
+          name="people"
+          placeholder="Number of People"
+          value={values.people}
+          handleChange={handleChange}
+          min="1"
+        ></FormRow>
         <button className="btn btn-blok" type="submit">
           <h5>Submit</h5>
         </button>
