a0ad1ebd22b367aba4b6b44ca2eda7bb6c16c9e7
HaroldDRF <95robots@gmail.com>
2022-06-22 23:02:39 -0700

    date validation added to create reserveation api

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,26 @@
 const hasProperties = require("../errors/hasProperties");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+// UTILS
+const checkDate = (date, time) => {
+  const inputDate = new Date(date);
+  const inputTime = new Date(time);
+
+  const currentDate = new Date();
+
+  const currentTime = currentDate.getTime();
+
+  console.log(inputDate, inputTime, inputDate >= currentDate);
+
+  return false;
+};
+
 // CONTROLLERS
 const service = require("./reservation.service");
 
 async function list(req, res) {
   const { date } = req.query;
-  console.log(date);
+
   const data = await service.list(date);
   res.json({
     data: data,
@@ -15,10 +29,22 @@ async function list(req, res) {
 
 async function create(req, res) {
   const data = req.body.data;
-  const reservation = await service.create(data);
-  return res.json({
-    data: reservation,
-  });
+  const {
+    first_name,
+    last_name,
+    mobile_number,
+    reservation_date,
+    reservation_time,
+    people,
+  } = data;
+
+  if (checkDate(reservation_date, reservation_time) === true) {
+    const reservation = await service.create(data);
+    return res.json({
+      data: reservation,
+    });
+  }
+  res.status(404).json({ message: `It has to be a future date` });
 }
 
 module.exports = {
