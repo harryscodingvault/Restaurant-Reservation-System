8bce228fdf3ecb8e05996a93eb0a9e7f3615cd2c
HaroldDRF <95robots@gmail.com>
2022-06-23 22:09:40 -0700

    create and list tables route created

--- back-end/src/tables/table.controller.js
+++ back-end/src/tables/table.controller.js
@@ -5,9 +5,7 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const service = require("./table.service");
 
 async function list(req, res) {
-  const { date } = req.query;
-
-  const data = await service.list(date);
+  const data = await service.list();
   res.json({
     data: data,
   });
@@ -15,24 +13,11 @@ async function list(req, res) {
 
 async function create(req, res) {
   const data = req.body.data;
-  const {
-    first_name,
-    last_name,
-    mobile_number,
-    reservation_date,
-    reservation_time,
-    people,
-  } = data;
 
-  if (checkDate(reservation_date, reservation_time) === true) {
-    const reservation = await service.create(data);
-    return res.json({
-      data: reservation,
-    });
-  }
-  res
-    .status(404)
-    .json({ message: checkDate(reservation_date, reservation_time) });
+  const reservation = await service.create(data);
+  return res.json({
+    data: reservation,
+  });
 }
 
 module.exports = {
