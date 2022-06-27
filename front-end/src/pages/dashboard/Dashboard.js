9dd285afdcc48ddd457a1221d84a6dc738b6b2aa
HaroldDRF <95robots@gmail.com>
2022-06-27 12:43:24 -0700

    search by date working on dahsboard

--- front-end/src/pages/dashboard/Dashboard.js
+++ front-end/src/pages/dashboard/Dashboard.js
@@ -5,18 +5,23 @@ import ReservationList from "../../layout/ReservationList";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getAllReservations } from "../../features/reservation/reservationSlice";
+import { today } from "../../utils/date-time";
+
+const initialState = {
+  currentDate: today(),
+};
 
 function Dashboard() {
   const { reservation_list } = useSelector((store) => store.reservation);
   const [reservationsError, setReservationsError] = useState(null);
-  const [searchByDate, setSearchByDate] = useState(null);
+  const [searchByDate, setSearchByDate] = useState(initialState.currentDate);
   const [searchByPhone, setSearchByPhone] = useState(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const loadDashboard = () => {
       const abortController = new AbortController();
-      dispatch(getAllReservations());
+      dispatch(getAllReservations(searchByDate));
       setReservationsError(null);
 
       return () => abortController.abort();
