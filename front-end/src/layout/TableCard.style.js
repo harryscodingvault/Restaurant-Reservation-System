9471273a94ea7b14b5e6815f5cf1b119984bf25e
HaroldDRF <95robots@gmail.com>
2022-06-28 10:23:27 -0700

    free/occupied status added to table cards

--- front-end/src/layout/TableCard.style.js
+++ front-end/src/layout/TableCard.style.js
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  .row {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    .text-group {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      width: 33%;
+
+      .label {
+        color: var(--primary-500);
+      }
+    }
+  }
+`;
 
 export default Wrapper;
