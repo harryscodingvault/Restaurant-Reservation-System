803c2a9847d72be62774f4fd2e40825315f2debd
HaroldDRF <95robots@gmail.com>
2022-07-05 13:39:14 -0700

    backend: story 4 tests passes

--- back-end/src/tables/table.controller.js
+++ back-end/src/tables/table.controller.js
@@ -84,7 +84,7 @@ async function update(req, res) {
   const { reservation_id, people } = reservation;
 
   if (table.reservation_id !== null) {
-    return res.status(200).json({ message: "table is already occupied" });
+    return res.status(400).json({ error: "table is already occupied" });
   }
   if (capacity >= people) {
     const data = await service.update({ table_id, reservation_id });
@@ -96,7 +96,7 @@ async function update(req, res) {
       data: data,
     });
   }
-  return res.status(400).json({ message: "reservation is over capacity" });
+  return res.status(400).json({ error: "reservation is over capacity" });
 }
 
 async function deleteTable(req, res) {
