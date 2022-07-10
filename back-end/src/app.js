2aaae078cd640fe88acc7be209a6635078319174
HaroldDRF <95robots@gmail.com>
2022-07-10 01:33:03 -0700

    prod

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,6 +4,8 @@ require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
@@ -12,11 +14,19 @@ const tablesRouter = require("./tables/table.router");
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "./build")));
 app.use(cors());
+app.use(xss());
+app.use(helmet());
 app.use(express.json());
 
 app.use("/reservations", reservationsRouter);
 app.use("/tables", tablesRouter);
+
+app.get("*", function (req, res) {
+  const index = path.join(__dirname, "build", "index.html");
+  res.sendFile(index);
+});
 
 app.use(notFound);
 app.use(errorHandler);
